bed2495208904d7b8d311473ac8fb0bbb2f16265
Noemi Radvan <radvanno@gmail.com>
2022-02-17 15:14:46 +0100

    email.js added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,30 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const formRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_zp6ldyp",
+        "template_ddb79jj",
+        formRef.current,
+        "user_5cJt7ENIx4q3Tu8MlKD1s"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="contact">
       <div className="contact-background"></div>
@@ -31,7 +53,7 @@ const Contact = () => {
             contribution!
           </p>
 
-          <form>
+          <form ref={formRef} onSubmit={handleSubmit}>
             <input type="text" placeholder="Name" name="user_name" />
             <input type="text" placeholder="Subject" name="user_subject" />
             <input type="text" placeholder="Email" name="user_email" />
